4f3f920b84e28d6e7fd188b20adc1c7b33bed491
Patrick Bélanger <1848500+patrickbelanger@users.noreply.github.com>
2021-01-02 16:57:15 -0500

    * WIP - Give proper naming on methods of the Holding Controller * Update unit test

--- src/main/java/org/patrickbelanger/investment/tool/controller/HoldingController.java
+++ src/main/java/org/patrickbelanger/investment/tool/controller/HoldingController.java
@@ -19,8 +19,8 @@ package org.patrickbelanger.investment.tool.controller;
 
 import java.util.List;
 
-import org.patrickbelanger.investment.tool.model.Portfolio;
-import org.patrickbelanger.investment.tool.service.PortfolioService;
+import org.patrickbelanger.investment.tool.model.Holding;
+import org.patrickbelanger.investment.tool.service.HoldingService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -33,36 +33,36 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
- * PortfolioController class
+ * HoldingController class
  * 
  * @author Patrick Belanger
  *
  */
 @RestController
-@RequestMapping("/api/portfolio")
+@RequestMapping("/api/holding")
 public class HoldingController {
 
   @Autowired
-  private PortfolioService portfolioService;
+  private HoldingService holdingService;
   
   @PostMapping("/")
-  public void addPortfolio(@RequestBody Portfolio newPortfolio) {
-    portfolioService.addPortfolio(newPortfolio);
+  public void addHolding(@RequestBody Holding newHolding) {
+    holdingService.addHolding(newHolding);
   }
   
   @GetMapping("/all")
-  public ResponseEntity<List<Portfolio>> getPortfolios() {
-   return portfolioService.getPortfolios();
+  public ResponseEntity<List<Holding>> getHoldings() {
+   return holdingService.getHoldings();
   }
   
   @PutMapping("/{id}")
-  public int updatePortfolio(@PathVariable("id") int id, @RequestBody Portfolio portfolio) {
-    return portfolioService.updatePortfolio(id, portfolio);
+  public int updatePortfolio(@PathVariable("id") int id, @RequestBody Holding holding) {
+    return holdingService.updateHolding(id, holding);
   }
     
   @DeleteMapping("/{id}")
   public int deletePortfolio(@PathVariable("id") int id) {
-    return portfolioService.deletePortfolio(id);
+    return holdingService.deleteHolding(id);
   }
   
 }
